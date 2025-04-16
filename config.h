72b022764e5bbd247b301be3ca558b905794ea68
josejosepreso <josehihat@gmail.com>
2025-04-16 12:44:44 -0600

    tab

--- config.h
+++ config.h
@@ -69,55 +69,54 @@ static const char *termcmd[]  = { "urxvt" , NULL };
 static const char *ncmpcpp[] = { "st", "-e", "ncmpcpp", NULL };
 
 static const Key keys[] = {
-	{ MODKEY|ShiftMask,             XK_m,     zoom,            {0} },
-	{ MODKEY,             		XK_v,     music_tag, 	   {0} },
-	{ MODKEY,             		XK_c,     spawn, 	   SHCMD("urxvt -e ranger") },
-	{ MODKEY,                       XK_e,     spawn, 	   SHCMD("emacs") },
-	{ 0,				XK_Print, spawn, 	   SHCMD("flameshot gui") },
-	{ 0,				XF86XK_AudioLowerVolume,   spawn, SHCMD("mpc volume -5") },
-	{ 0,				XF86XK_AudioRaiseVolume,   spawn, SHCMD("mpc volume +5") },
-	{ 0,				XF86XK_AudioPlay, spawn,   SHCMD("mpc toggle") },
-	{ 0, 				XF86XK_AudioNext, spawn,   SHCMD("mpc next") },
-	{ 0, 				XF86XK_AudioPrev, spawn,   SHCMD("mpc prev") },
-	{ 0, 				XF86XK_MonBrightnessUp,    spawn, SHCMD("/usr/bin/brightnessctl set +3%") },
-	{ 0, 				XF86XK_MonBrightnessDown,  spawn, SHCMD("/usr/bin/brightnessctl set 3%-") },
-	{ MODKEY|ShiftMask,             XK_d,      spawn,	   SHCMD("bookmarks") },
-
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ Mod1Mask,                     XK_Tab,    view,           {0} },
-	{ MODKEY,			XK_w,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY,                       XK_Tab,    tagswapmon,	   {0} },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-      	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key        			function        argument */
+	{ MODKEY|ShiftMask,             XK_m,     			zoom,           {0} },
+	{ MODKEY,             		XK_v,     			music_tag, 	{0} },
+	{ MODKEY,             		XK_c,     			spawn, 	   	SHCMD("urxvt -e ranger") },
+	{ MODKEY,                       XK_e,     			spawn, 	   	SHCMD("emacs") },
+	{ 0,				XK_Print, 			spawn, 	   	SHCMD("flameshot gui") },
+	{ 0,				XF86XK_AudioLowerVolume,   	spawn, 		SHCMD("mpc volume -5") },
+	{ 0,				XF86XK_AudioRaiseVolume,   	spawn, 		SHCMD("mpc volume +5") },
+	{ 0,				XF86XK_AudioPlay, 		spawn,   	SHCMD("mpc toggle") },
+	{ 0, 				XF86XK_AudioNext, 		spawn,   	SHCMD("mpc next") },
+	{ 0, 				XF86XK_AudioPrev, 		spawn,   	SHCMD("mpc prev") },
+	{ 0, 				XF86XK_MonBrightnessUp,    	spawn, 		SHCMD("/usr/bin/brightnessctl set +3%") },
+	{ 0, 				XF86XK_MonBrightnessDown,  	spawn, 		SHCMD("/usr/bin/brightnessctl set 3%-") },
+	{ MODKEY|ShiftMask,             XK_d,      			spawn,	   	SHCMD("bookmarks") },
+	{ MODKEY,                       XK_p,      			spawn,          {.v = dmenucmd } },
+	{ MODKEY,             		XK_Return, 			spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      			togglebar,      {0} },
+	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      			incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      			incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      			setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      			setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, 			zoom,           {0} },
+	{ Mod1Mask,                     XK_Tab,    			view,           {0} },
+	{ MODKEY,			XK_w,      			killclient,     {0} },
+	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  			setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  			togglefloating, {0} },
+	{ MODKEY,                       XK_0,      			view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      			tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  			focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 			focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_Tab,    			tagswapmon,	   {0} },
+	{ MODKEY|ShiftMask,             XK_comma,  			tagmon,         {.i = -1 } },
+      	{ MODKEY|ShiftMask,             XK_period, 			tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,      			                0)
+	TAGKEYS(                        XK_2,      			                1)
+	TAGKEYS(                        XK_3,      			                2)
+	TAGKEYS(                        XK_4,      			                3)
+	TAGKEYS(                        XK_5,      			                4)
+	TAGKEYS(                        XK_6,      			                5)
+	TAGKEYS(                        XK_7,      			                6)
+	TAGKEYS(                        XK_8,      			                7)
+	TAGKEYS(                        XK_9,      			                8)
+	{ MODKEY|ShiftMask,             XK_q,      			quit,           {0} },
 };
 
 /* button definitions */
